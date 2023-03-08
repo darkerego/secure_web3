382a1b9317012fda94232292f584b3a4f8e79f78
darkerego <xelectrono@protonmail.com>
2023-03-08 00:23:51 -0500

    package

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ from setuptools import setup
 
 setup(
     name='secure_web3',
-    version='1.1',
-    packages=[''],
-    install_requires=['web3', 'pycryptodome'],
-    data_files=[('keys', ['keys/default_wallet.json']), ('data', ['data/networks.json'])],
+    version='1.15',
+    packages=['secure_web3'],
+    install_requires=['web3', 'pycryptodome', 'dotenv', 'eth-utils'],
+    package_dir={"": "src"},
+    include_package_data=True,
+    data_files=[('', ['src/secure_web3/keys/default_wallet.json', 'src/secure_web3/data/networks.json'])],
     url='https://github.com/darkerego/secure_web3',
     license='MIT',
     author='darkerego',
